66f914c7f5a4e98c307099ed7ffbe15fba8607e0
Demouser906 <144139319+Shielaapdo@users.noreply.github.com>
2024-02-04 22:30:37 +0800

    changes

--- modal.js
+++ modal.js
@@ -1,60 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="device-width, initial-scale=1.0"/>
-    <title>homework 4</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&family=Roboto:wght@400;500;700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css"/>
-
-  </head>
-  <body>       
-    <script src=".js/modal.js"></script>           
-    <header class="header-list">
-      <div class="list-list">
-      <nav>
-        <a class="logo-list"><span class="web-logo">WEB</span><span class="studio-logo">STUDIO</span></a>       
-              <ul class="list">
-                 <li class="list-1">
-                    <a>Studio</a>
-                      </li> 
-                        <li class="list-1">
-                           <a>Portfolio</a>
-                           </li>
-                              <li class="list-1">
-                                <a>Email</a>
-                                  </li>
-                                    <li class="list-1">
-                                <a>Contact</a>
-                              </li>
-                          </ul>            
-                    </div>
-                          <div class="list-2">
-                                  <ul>
-                                       <li class="list-1">
-                                          <a>info@devstudio.com</a>
-                                            </li>
-                                          <li class="list-1">
-                                             <a>+11 (000) 111- 11- 11</a>
-                                          </li>           
-                                  </ul>
-                          </div>
-         </nav>
-   </div>
-                        
-    </header>
-    <div class="office">
-         <div class="overlay">
-           <ul>
-                <p class="solution">
-                Effective Solution <br/>
-                for your business
-                </p>
-                <button class="btn-button">Order Services</button>
-          </ul>
-        </div>
-   </div>
-   </html>
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
