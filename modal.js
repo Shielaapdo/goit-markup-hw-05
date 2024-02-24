d3e99fe00c5d1d37711fad0316144a8bf7dcf9fe
Demouser906 <144139319+Shielaapdo@users.noreply.github.com>
2024-02-24 11:34:12 +0800

    new update

--- modal.js
+++ modal.js
@@ -2,30 +2,15 @@ js/modal.js
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector(".data-modal-open"),
-    closeModalBtn: document.querySelector(".data-modal-close"),
-    modal: document.querySelector(".data-modal-open"),
-  
-
-    /*additional code*/
-    modal : document.querySelector(".modal"),
-    btnonclick: function() {
-    modal.style.display = "block";
-  }
-}
-
-  /* imageSF.onclick = OpenModal()
-  openModal.addEventListener ('onclick',() => {
-    modal.showModal();
-  }
-  CloseModal.addEventListener ('onclick',() => {
-    modalonclick.closemodal();
-  }*/
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+
   function toggleModal() {
-  refs.modal.classList.toggle("is-hidden");;
-    
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
